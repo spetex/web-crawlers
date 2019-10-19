073082b652e9b4b80cde168ba4af6a180561e235
Petr Sahula <spetex@icloud.com>
2019-10-19 12:37:11 +0200

    Finished crawler - server prototype

---
+++ crawler.rb
@@ -0,0 +1,38 @@
+require 'kimurai'
+require 'json'
+
+LINK_DOMAIN = "https://goout.net"
+FILE_NAME = "data/goout_newly_announced.json"
+
+
+class GoOutSpider < Kimurai::Base
+  @name = "GoOut_Spider"
+  @engine = :mechanize
+  @start_urls = ["https://goout.net/cs/praha/akce/?sort=newly_announced"]
+
+  def parse(response, url:, data: {})
+    file = File.open FILE_NAME
+    loaded = JSON.load file
+    file.close
+
+    countUpdated = 0
+
+    response.css('.eventCard .info').each do |card|
+      event = {
+        :name => card.css('span[itemprop=name].name').text.squish,
+        :link => "#{LINK_DOMAIN}#{card.css('a').attribute('href')}",
+        :venue => card.css('.venue span[itemprop=name]').text.squish,
+        :venueLink => "#{LINK_DOMAIN}#{card.css('.venue span[itemprop=geo]').attribute('data-venue-href')}",
+        :dateTime => card.css('time').attribute('datetime'),
+        :scrapeDate => Time.now.to_s,
+      }
+      found = loaded.detect { |item| item["link"] == event[:link] }
+      event[:scrapeDate] = found["scrapeDate"] if found
+      count += 1 unless found
+      save_to FILE_NAME, event, format: :pretty_json
+    end
+    puts "Update #{countUpdated} items."
+  end
+end
+
+GoOutSpider.crawl!
---
+++ server.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'rss'
+require 'json'
+
+def produceRSS(events)
+  rss = RSS::Maker.make("atom") do |maker|
+    maker.channel.author = "GoOut"
+    maker.channel.updated = Time.now.to_s
+    maker.channel.about = "List of just announced events in Prague"
+    maker.channel.title = "GoOut.cz Praha Newly Announced"
+
+    events.each do |event|
+      maker.items.new_item do |item|
+        item.link = event["link"]
+        item.title = event["name"]
+        item.updated = event["scrapeDate"]
+      end
+    end
+  end
+end
+
+get '/goout.rss', :provides => ['rss', 'atom', 'xml'] do
+  file = File.open "data/goout_newly_announced.json"
+  events = JSON.load file
+  rss = produceRSS events
+  "#{rss}"
+end
+
+
