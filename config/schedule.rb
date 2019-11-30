6662892be5cc63d7362bbbc401980062453698f5
Petr Sahula <spetex@icloud.com>
2019-11-30 18:13:57 +0100

    Add ruby version to cron command

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,9 +3,10 @@ env 'CRAWLERS', ENV['CRAWLERS_DIR']
 env 'CRAWLERS_ENV', ENV['CRAWLERS_RUBY_GEMSET']
 env 'CRAWLERS_DATA_DIR', ENV['CRAWLERS_DATA_DIR']
 env 'CRAWLERS_LOG_FILE', ENV['CRAWLERS_LOG_FILE']
+env 'RUBY_VERSION', ENV['RUBY_VERSION']
 
 every 30.minutes do
-  command "exec rvm ruby@$CRAWLERS_ENV do ruby $CRAWLERS/goout.rb >> $CRAWLERS_LOG_FILE"
+  command "exec rvm $RUBY_VERSION@$CRAWLERS_ENV do ruby $CRAWLERS/goout.rb >> $CRAWLERS_LOG_FILE"
 end
 
 # Learn more: http://github.com/javan/whenever
